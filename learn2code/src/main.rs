2f32f9dbf5a7a89c28149ffeae1203115fc57e04
william <william.wow@icloud.com>
2024-03-19 11:59:26 +0800

    implement gcd

--- learn2code/src/main.rs
+++ learn2code/src/main.rs
@@ -1,3 +1,58 @@
+use std::env;
+mod utils;
+use crate::utils::gcd;
+use std::str::FromStr;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to use");
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() <= 2 {
+        print_help();
+    }
+
+    let cmd = &args[1];
+    match cmd.as_str() {
+        "-h" | "--help" => print_help(),
+        "-v" | "--version" => print_version(),
+        "gcd" => gcd_cmd_handler(&args[2..]),
+        _ => print_help(),
+    }
+}
+
+fn print_help() {
+    println!("Usage: gcd num1 num2 ...numn");
+}
+
+fn print_version() {
+    println!("learn2code 0.1.0");
+}
+
+fn gcd_cmd_handler(args: &[String]) {
+    let mut nums = Vec::new();
+    for arg in args {
+        nums.push(u64::from_str(arg).expect("invalid number"));
+    }
+
+    if nums.len() == 0 {
+        eprintln!("no number given");
+        std::process::exit(1);
+    }
+
+    let mut d = nums[0];
+    for m in &nums[1..] {
+        d = gcd(d, *m);
+    }
+
+    println!(
+        "gcd({}) = {}",
+        format!(
+            "{}",
+            nums.iter()
+                .map(|&n| n.to_string())
+                .collect::<Vec<_>>()
+                .join(", ")
+        ),
+        d
+    )
 }
